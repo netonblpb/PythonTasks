b08d7c424d8d1fdbbdf03c5fc492e470d98f9da5
netonblpb <skullbog@gmail.com>
2022-05-19 11:50:49 +0300

    Task41 apdated

--- Task41.py
+++ Task41.py
@@ -7,33 +7,35 @@
 # 12W1B12W3B24W1B14W
 
 proc = input('Выберите процесс (0 - ахивирование, 1 - восстановление): ')
+arch = open('ArchiveIn.txt', 'r')
+unarch = open('ArchiveOut.txt', 'r')
 
 if int(proc) == 0:
-    result = ''
+    resultin = ''
     count = 1
-    string = input('Введите строку для архивирования: ')
+    string = unarch.readline()
 
     for i in range(0, len(string) - 1):        
         if i <= len(string) - 1:
             if string[i] == string[i+1]:
                 count += 1
             else:
-                result += str(count) + string[i]
+                resultin += str(count) + string[i]
                 count = 1
         if i == len(string) - 2 and string[i] == string[i+1]:
-            result += str(count) + string[i]
+            resultin += str(count) + string[i]
         if i == len(string) - 2 and string[i] != string[i+1]:
-            result += str(count) + string[i]
-            result += '1' + string[i+1]
-    print(result)
+            resultin += str(count) + string[i]
+            resultin += '1' + string[i+1]
+    print(resultin)    
 
 numberInt = []
 letterStr = []
 
 if int(proc) == 1:
-    string = input('Введите строку для восстановления: ')
+    string = arch.readline()
     num = ''
-    result = ''
+    resultout = ''
 
     for i in range (0, len(string)):
         if string[i].isdigit():
@@ -44,9 +46,11 @@ if int(proc) == 1:
             letterStr.append(string[i])
     
     for i in range (0, len(letterStr)):
-        result += letterStr[i] * int(numberInt[i])
+        resultout += letterStr[i] * int(numberInt[i])
+    print(resultout)
 
 elif int(proc) != 0 and int(proc) != 1:
     print('Неверное условие!')
 
-print(result)
+arch.close()
+unarch.close()
